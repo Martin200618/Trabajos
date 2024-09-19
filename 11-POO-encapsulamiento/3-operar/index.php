96b38011485e0417e8bf44da10d4e5ccdba6e5d3
Martin <martinstibennarvez2006@gmail.com>
2024-09-19 11:17:04 -0500

    trabajo

--- 11-POO-encapsulamiento/3-operar/index.php
+++ 11-POO-encapsulamiento/3-operar/index.php
@@ -5,21 +5,27 @@ include("./libreria/operacion.php");
 $num1 = new Numero(10);
 $num2 = new Numero(5);
 
-$operacio = new Operacion($num1, $num2);
+$operacion = new Operacion($num1, $num2);
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Operacion</title>
 </head>
+
 <body>
-    <?php
-    echo "<p>Suma: ". $operacio->sumar(). "</p>";
-    echo "<p>Resta: ". $operacio->restar(). "</p>";
-    echo "<p>Multiplicacion: ". $operacio->multiplicar(). "</p>";
-    echo "<p>Division: ". $operacio->dividir(). "</p>";
-    ?>
+
+<?php
+    echo "<p>Suma: ". $operacion->sumar(). "</p>";
+    echo "<p>Resta: ". $operacion->restar(). "</p>";
+    echo "<p>Multiplicacion: ". $operacion->multiplicar(). "</p>";
+    echo "<p>Division: ". $operacion->dividir(). "</p>";
+?>
+
 </body>
-</html>
+
+</html>         
